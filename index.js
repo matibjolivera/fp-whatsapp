a637b994570ef90e61d285de70ef312b402cec24
Matias Olivera <matibjolivera@gmail.com>
2021-03-21 03:38:55 -0300

    changes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// Supports ES6
-// import { create, Whatsapp } from 'venom-bot';
 const venom = require('venom-bot');
 const fetch = require("node-fetch");
-const API_URL = 'https://fp-woocommerce.herokuapp.com/orders';
+const API_URL = 'https://fp-woocommerce.herokuapp.com/orders/';
 
 venom
     .create()
@@ -14,28 +12,31 @@ venom
 async function sendMessages(client) {
     const clientSender = client;
     await (async function () {
-        let clients = await getClients();
-        for (let e of clients.result) {
-            let phone = e.billing.phone;
-            if (!e.billing.phone.match(/^(54)/)) {
+        let orders = await getOrders();
+        for (let order of orders.result) {
+            let billingClient = order.billing;
+            let phone = billingClient.phone;
+            if (!phone.match(/^(54)/)) {
                 phone = '54' + phone
             }
             console.log("Contacto: " + phone + '@c.us');
 
-            await clientSender
-                .sendText(phone + '@c.us', 'Test bot!')
-                .then((result) => {
-                    console.log('Result: ', result); //return object success
-                })
-                .catch((error) => {
-                    console.error('Error when sending: ', error); //return object error
-                });
+            //if ('541134160701' === phone) { //TODO: Delete if
+                await clientSender
+                    .sendText(phone + '@c.us', 'Test bot!')
+                    .then((result) => {
+                        console.log('Result: ', result);
+                    })
+                    .catch((error) => {
+                        console.error('Error when sending: ', error);
+                    });
+            //}
         }
         setTimeout(arguments.callee, 60000);
     })();
 }
 
-async function getClients() {
+async function getOrders() {
     let response = await fetch(API_URL);
     return await response.json();
 }
